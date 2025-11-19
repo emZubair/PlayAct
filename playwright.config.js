33e0259508e9e96b815ca6b7d1b2c18e848b79a8
Zubair <36886070+emZubair@users.noreply.github.com>
2025-11-20 01:07:47 +0500

    Fix allure report GitHub issue (#1)

--- playwright.config.js
+++ playwright.config.js
@@ -6,10 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [
-    ['html'],
-    ['allure-playwright', { outputFolder: 'allure-results' }]
-  ],
+  reporter: 'html',
   use: {
     baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
